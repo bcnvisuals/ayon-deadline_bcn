7ea00f7f06cb08ec87c8d4c015b619dac2e7af23
George Antonopoulos <george.antonopoulos@bcnvisuals.com>
2025-12-01 11:35:56 +0000

    Update version to 0.5.15-bcn.9 in package.py and version.py

--- client/ayon_deadline/version.py
+++ client/ayon_deadline/version.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'deadline' version."""
+<<<<<<< HEAD
 __version__ = "0.5.19-bcn.1"
+=======
+__version__ = "0.5.15-bcn.9"
+>>>>>>> 2a15d45 (Update version to 0.5.15-bcn.9 in package.py and version.py)
